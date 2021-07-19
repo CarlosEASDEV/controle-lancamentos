5e35b0f8f3b2640a8dd7c18b5c8a1212c4177e61
carlosesousa <ce.sousa@gmail.com>
2021-07-18 22:26:35 -0300

    packages 11

--- src/app/tabela-lancamentos/tabela-lancamentos.component.spec.ts
+++ src/app/tabela-lancamentos/tabela-lancamentos.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TabelaLancamentosComponent } from './tabela-lancamentos.component';
 
@@ -6,7 +6,7 @@ describe('TabelaLancamentosComponent', () => {
   let component: TabelaLancamentosComponent;
   let fixture: ComponentFixture<TabelaLancamentosComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TabelaLancamentosComponent ]
     })
